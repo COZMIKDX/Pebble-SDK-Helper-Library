4df8c5c2ec4b28e9880bab2047b9fe196c67e00c
COZMIKDX <tacocatdx@gmail.com>
2023-07-22 19:32:45 -0500

    started image struct files

---
+++ Image.c
@@ -0,0 +1,13 @@
+#include <pebble.h>
+#include "mainWindow.h"
+
+struct Image {
+    uint32_t resource_id;
+    GRect image_dimension_position;
+    Gbitmap * image;
+    BitmapLayer * image_layer;
+}
+
+static void Image(GRect spatialInfo, uint32_t resource_id) {
+
+}
