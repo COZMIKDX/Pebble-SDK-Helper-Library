cf614f4b9146c429ddc7134b96cf2a35efcf7d26
COZMIKDX <tacocatdx@gmail.com>
2023-07-24 21:58:57 -0500

    Creating struct that will contain an array of pointers to image structs which will contain the variables needed for dealing with images. I'm still in the middle of that but I'm switching computers.

--- Image.c
+++ Image.c
@@ -7,15 +7,19 @@ struct Image {
     BitmapLayer * image_layer;
 }
 
-static Image * create_image(GRect spatialInfo, uint32_t resource_id) {
+static Image * init_image(struct Image * image_struct, GRect spatialInfo, uint32_t resource_id) {
     
-    image_dimension_position = spatialInfo;
-    image_resource_id = resource_id;
+    image_struct->image_dimension_position = spatialInfo;
+    image_struct->image_resource_id = resource_id;
 
-    image = gbitmap_create_with_resource(image_resource_id);
-    image_layer = bitmap_layer_create(image_dimension_position);
-    bitmap_layer_set_compositing_mode(image_layer, GCompOpSet);
-    bitmap_layer_set_bitmap(image_layer, image);
+    image_struct->image = gbitmap_create_with_resource(image_resource_id);
+    image_struct->image_layer = bitmap_layer_create(image_dimension_position);
+    bitmap_layer_set_compositing_mode(image_struct->image_layer, GCompOpSet);
+    bitmap_layer_set_bitmap(image_struct->image_layer, image_struct->image);
     // Add the bitmap layer to the window layer in the Images.c file.
+}
 
+static void destroy_image_struct(struct Image * image_struct) {
+    gbitmap_destroy(image_struct->image);
+    bitmap_layer_destroy(image_struct->image_layer);
 }
--- Images.c
+++ Images.c
@@ -1,5 +1,28 @@
 #include <pebble.h>
+#include "Image.h"
 
 struct Images {
-    
+    struct image ** image_array;  // array of pointers
+    uint32_t length = 0;
+    uint32_t top = 0;
+}
+
+static void init_images_struct(struct Images * image_list, uint32_t number_of_images) {
+    // Access the pointer for the array of pointers and have it point to the memory just allocated.
+    image_list->image_array = (struct Image *) malloc(sizeof(struct Image) * number_of_images);
+    image_list->length = number_of_images;
+}
+
+static void push_image(struct Images * image_list, struct Image * input_image) {
+    // Access the pointer for the array on pointers and then write the new image pointer value to the next unused slot. 
+    (image_list->image_array)[image_list->top] = input_image;
+}
+
+// Free what the image struct pointers are pointing at and then free the array holding the pointers.
+// I think I should call a function for the image structs that will free all of their own member pointers.
+static void de_init_images_struct(struct Images * image_list) {
+    for (int i = 0; i < image_list->length; i++) {
+        free((image_list->image_array)[i])
+    }
+    free(image_list->image_array);
 }
