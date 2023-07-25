a442e151736b708920264785e51ef7fd8d4f2f21
CosmicTacoCat <tacocatdx@gmail.com>
2023-07-25 12:53:06 -0500

    Fixed comparison with int and uint32_t.

--- Images.c
+++ Images.c
@@ -16,18 +16,21 @@ static void add_image(struct Images *image_list, GRect bounds, uint32_t resource
 }
 
 static void push_image(struct Images * image_list, struct Image * input_image) {
-    // Access the pointer for the array on pointers and then write the new image pointer value to the next unused slot. 
-    if (image_list->top != (length - 1)) {
+    // Access the pointer for the array on pointers and then write the new image pointer value to the next unused slot.
+    if (image_list->top != (image_list->length - 1))
+    {
         (image_list->image_array)[image_list->top] = input_image;
         image_list->top = image_list->top + 1;
-    } else {
+    }
+    else
+    {
         return; // Too many images for the current array. I'll make an array allocate and copy function later.
     }
 }
 
 // Free what the image struct pointers are pointing at and then free the array holding the pointers.
 static void de_init_images_struct(struct Images * image_list) {
-    for (int i = 0; i < image_list->length; i++) {
+    for (uint32_t i = 0; i < image_list->length; i++) {
         // Destroy the image, the image_struct, and then the pointer to the image struct.
         destroy_image_struct((image_list->image_array)[i]);
         free((image_list->image_array)[i]);
