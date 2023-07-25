2a9300591a752fb17b22e7dcff37d87e9490f077
CosmicTacoCat <tacocatdx@gmail.com>
2023-07-25 11:42:39 -0500

    Created/ edited functions related to creating image objects.

--- Images.c
+++ Images.c
@@ -13,16 +13,27 @@ static void init_images_struct(struct Images * image_list, uint32_t number_of_im
     image_list->length = number_of_images;
 }
 
+static void add_image(struct Images * image_list, GRect bounds, uint32_t resource_id) {
+    struct Image * image_struct = init_image(bounds, resource_id);
+    push_image(image_list, image_struct);
+}
+
 static void push_image(struct Images * image_list, struct Image * input_image) {
     // Access the pointer for the array on pointers and then write the new image pointer value to the next unused slot. 
-    (image_list->image_array)[image_list->top] = input_image;
+    if (top != (length - 1)) {
+        (image_list->image_array)[image_list->top] = input_image;
+        top = top + 1;
+    } else {
+        return; // Too many images for the current array. I'll make an array allocate and copy function later.
+    }
 }
 
 // Free what the image struct pointers are pointing at and then free the array holding the pointers.
-// I think I should call a function for the image structs that will free all of their own member pointers.
 static void de_init_images_struct(struct Images * image_list) {
     for (int i = 0; i < image_list->length; i++) {
-        free((image_list->image_array)[i])
+        // Destroy the image, the image_struct, and then the pointer to the image struct.
+        destroy_image_struct((image_list->image_array)[i]);
+        free((image_list->image_array)[i]);
     }
     free(image_list->image_array);
 }
