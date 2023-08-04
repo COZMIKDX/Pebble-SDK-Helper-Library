8e49d2128f0e3e69306668a0189c3ae9d672e3a7
CosmicTacoCat <tacocatdx@gmail.com>
2023-08-04 12:22:48 -0500

    finished function parameters

--- Text.h
+++ Text.h
@@ -2,10 +2,11 @@
 #include <pebble.h>
 
 struct Text {
-    GRect dimension_position;
+    GRect dimension_position; // still deciding if I want to keep this.
     char * text;
     TextLayer * text_layer;
 }
 
 struct * init_text(GRect spatial_info, char * text, GColor text_color, GColor bg_color, GFont font, layer * window_layer);
+void update_text(struct Text * text_struct, char * text);
 void destroy_text_struct(struct Text * text_struct);
