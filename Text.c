e80f75054de6bc34e735bc2fa58f641224f34a2f
CosmicTacoCat <tacocatdx@gmail.com>
2023-08-04 13:49:27 -0500

    fix accessing non-existant struct member

--- Text.c
+++ Text.c
@@ -12,7 +12,7 @@ struct Text * init_text(GRect spatial_info, char * text, GColor text_color, GCol
 
     text_layer_set_background_color(text_struct->text_layer, bg_color);
     text_layer_set_text_color(text_struct->text_layer, text_color);
-    text_layer_set_font(text_struct->text_layer, text_struct->font);
+    text_layer_set_font(text_struct->text_layer, font);
     text_layer_set_text_alignment(text_struct->text_layer, GTextAlignmentLeft);
     text_layer_set_text(text_struct->text_layer, text_struct->text); 
     layer_add_child(window_layer, text_layer_get_layer(text_struct->text_layer));
