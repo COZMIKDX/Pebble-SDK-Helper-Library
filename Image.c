b1c326f5dd5ab97bb2c6f3bda8fa6b6606787161
CosmicTacoCat <tacocatdx@gmail.com>
2023-08-08 12:03:29 -0500

    Added app_logs for finding malloc problem. Reorganize memory freeing

--- Image.c
+++ Image.c
@@ -24,10 +24,12 @@ struct Image * init_image(GRect spatial_info, uint32_t resource_id, Layer *windo
     bitmap_layer_set_bitmap(image_struct->image_layer, image_struct->image);
     layer_add_child(window_layer, bitmap_layer_get_layer(image_struct->image_layer));
 
+    APP_LOG(APP_LOG_LEVEL_INFO, "image struct created!");
     return image_struct;
 }
 
 void destroy_image_struct(struct Image * image_struct) {
     gbitmap_destroy(image_struct->image);
     bitmap_layer_destroy(image_struct->image_layer);
+    free(image_struct);
 }
